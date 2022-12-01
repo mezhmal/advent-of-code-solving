730bcb217186dbb42eda86babb50180663164dec
Mezhmal Sergey <slmezhmal@sberbank.ru>
2022-12-01 12:47:07 +0300

    day one / part two

--- day-01/solution.py
+++ day-01/solution.py
@@ -9,4 +9,15 @@ with open('calories.txt') as f:
         else:
             elf_inventory += int(value)
 
-print(max(calories))
+print(calories)
+
+print(calories.index(68442))
+print(calories.index(68177))
+print(calories.index(66575))
+
+calories.sort(reverse=True)
+
+print(calories)
+
+print(f"Max calories: {calories[0]}")
+print(f"Top three total calories: {sum(calories[:3])}")
