1bd26554421a14b6fb81e0fe8fead1394529c4aa
Mezhmal Sergey <s.mezhmal@gmail.com>
2022-12-02 17:05:56 +0300

    2021 / day 1 / part 2

--- aoc-2021/day-01/solution.py
+++ aoc-2021/day-01/solution.py
@@ -1,11 +1,30 @@
-count_of_increases = 0
-previous_measurement = 0
+measurements = []
+
 with open('input.txt') as f:
     for line in f.readlines():
-        measurement = int(line.strip())
-        if previous_measurement and previous_measurement < measurement:
-            count_of_increases += 1
+        measurements.append(int(line.strip()))
 
-        previous_measurement = measurement
+count_of_increases = 0
+previous_measurement = 0
+for measurement in measurements:
+    if previous_measurement and previous_measurement < measurement:
+        count_of_increases += 1
+
+    previous_measurement = measurement
 
 print(f"There are {count_of_increases} measurements that are larger than the previous measurement")
+
+measurements_length = len(measurements)
+count_of_increases = 0
+previous_measurement = 0
+
+for i in range(measurements_length):
+    if i < measurements_length - 2:
+        three_measurement_window = sum(measurements[i:i+3])
+        if previous_measurement and previous_measurement < three_measurement_window:
+            count_of_increases += 1
+
+        previous_measurement = three_measurement_window
+        # print(f"{i}: {measurements[i]} [{', '.join([str(m) for m in measurements[i:i+3]])}]")
+
+print(f"There are {count_of_increases} sums that are larger than the previous sum")
