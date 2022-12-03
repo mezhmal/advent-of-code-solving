5aecdf28eb2b7e397423ab2beab839cb0f16c0b4
Sergey Mezhmal <s.mezhmal@gmail.com>
2022-12-03 23:30:40 +0300

    2021 / day 2 / part 2

--- aoc-2021/day-02/solution.py
+++ aoc-2021/day-02/solution.py
@@ -1,22 +1,39 @@
 import os
 
 commands = []
+up = 'up'
+down = 'down'
+forward = 'forward'
+
 input_filename = 'input.txt'
 current_directory = os.path.dirname(__file__)
 
 with open(os.path.join(current_directory, input_filename)) as f:
     for line in f.readlines():
         direction, units = line.strip().split(' ')
-        commands.append({
-            'direction': direction,
-            'units': int(units)
-        })
+        commands.append((direction, int(units)))
 
 # solution for part 1
 
-horizontal_position = sum([command.get('units', 0) for command in commands if command.get('direction') == 'forward'])
-summary_moves_to_down = sum([command.get('units', 0) for command in commands if command.get('direction') == 'down'])
-summary_moves_to_up = sum([command.get('units', 0) for command in commands if command.get('direction') == 'up'])
+horizontal_position = sum([units for direction, units in commands if direction == forward])
+summary_moves_to_down = sum([units for direction, units in commands if direction == down])
+summary_moves_to_up = sum([units for direction, units in commands if direction == up])
 depth = summary_moves_to_down - summary_moves_to_up
 
 print(f"horizontal_position * depth = {horizontal_position * depth}")
+
+# solution for part 2
+
+aim = 0
+depth = 0
+horizontal_position = 0
+for direction, units in commands:
+    if direction == down:
+        aim += units
+    elif direction == up:
+        aim -= units
+    elif direction == forward:
+        horizontal_position += units
+        depth += aim * units
+
+print(f"(new interpretation of the commands) horizontal_position * depth = {horizontal_position * depth}")
