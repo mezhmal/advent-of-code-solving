70186e384a220f480214dd9f68396ffb31bb4526
Mezhmal Sergey <s.mezhmal@gmail.com>
2022-12-13 17:35:00 +0300

    2022 / day 11 / part 2

--- 2022/day-11/solution.py
+++ 2022/day-11/solution.py
@@ -1,7 +1,7 @@
 import math
 import os
 import time
-from typing import TypedDict
+from typing import TypedDict, Optional
 
 
 Monkey = TypedDict('Monkey', {
@@ -64,22 +64,26 @@ def read_notes_from_file(filename:str) -> Notes:
     return notes
 
 
-def get_new_worry_level(old:int, operation:str) -> int:
-    new = 0
+def get_new_worry_level(worry_level:int, operation:str, lcm:Optional[int]=None) -> int:
     operator, value = operation.split(' ')[-2:]
-    value = old if value == 'old' else int(value)
+    value = worry_level if value == 'old' else int(value)
     if operator == '+':
-        new = old + value
+        worry_level += value
     if operator == '*':
-        new = old * value
+        worry_level *= value
 
-    return math.floor(new / 3)
+    if lcm:
+        worry_level %= lcm
+    else:
+        worry_level //= 3
+
+    return worry_level
 
 
-def play_round(notes:Notes) -> Notes:
+def play_round(notes:Notes, lcm:Optional[int]=None) -> Notes:
     for monkey in notes:
         for item in monkey['items']:
-            worry_level = get_new_worry_level(item, monkey['operation'])
+            worry_level = get_new_worry_level(item, monkey['operation'], lcm)
             to_monkey = monkey['test']['if_true'] if worry_level % monkey['test']['divisible_by'] == 0 else monkey['test']['if_false']
             notes[to_monkey]['items'].append(worry_level)
 
@@ -89,15 +93,38 @@ def play_round(notes:Notes) -> Notes:
     return notes
 
 
+def get_level_of_monkey_business(notes:Notes) -> int:
+    most_inspected_items = sorted([monkey['inspected_items'] for monkey in notes])[-2:]
+    return most_inspected_items[0] * most_inspected_items[1]
+
+
 def main():
     input_filename = 'input.txt'
     current_directory = os.path.dirname(__file__)
+
+    # solution for part 1
+
     notes = read_notes_from_file(os.path.join(current_directory, input_filename))
-    for i in range(20):
+    for _ in range(20):
         notes = play_round(notes)
 
-    max_times = sorted([monkey['inspected_items'] for monkey in notes])[-2:]
-    print(f"(part 1) Level of monkey business: {max_times[0] * max_times[1]}")
+    print(f"(part 1) Level of monkey business after 20 rounds: {get_level_of_monkey_business(notes)}")
+
+    # solution for part 2
+
+    """
+    The main difficulity of part 2 is a need to find another way to keep worry levels manageable.
+    Because for many round worry level of items increased to huge values.
+    So we can get least common multiple (lcm) (see more information on https://en.wikipedia.org/wiki/Least_common_multiple)
+    of divisors from monkey tests and use it for reducing worry level after operations.
+    """
+
+    lcm = math.lcm(*[monkey['test']['divisible_by'] for monkey in notes])
+    notes = read_notes_from_file(os.path.join(current_directory, input_filename))
+    for _ in range(10 * 1000):
+        notes = play_round(notes, lcm)
+
+    print(f"(part 2) Level of monkey business after 10 000 rounds: {get_level_of_monkey_business(notes)}")
 
 
 if __name__ == "__main__":
