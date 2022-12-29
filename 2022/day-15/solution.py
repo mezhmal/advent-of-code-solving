301c9763363fe634b1e292d161e174f03da7db4f
Mezhmal Sergey <s.mezhmal@gmail.com>
2022-12-29 19:31:51 +0300

    2022 / day 15 / part 1

--- 2022/day-15/solution.py
+++ 2022/day-15/solution.py
@@ -52,45 +52,49 @@ def find_edges(report:Report) -> tuple[PositionX, PositionY, PositionX, Position
     return min_x, min_y, max_x, max_y
 
 
-def strip_report(report:Report, target_line:int) -> Report:
+def strip_report(report:Report, line:int) -> Report:
     '''
     we can exclude those sensors coverage area of which not crossing the target line
     that would greatly reduce size of map
     '''
+
     result:Report = []
     for record in report:
         sensor_position, _, distance = record
         _, sensor_position_y = sensor_position
-        if sensor_position_y - distance < target_line < sensor_position_y + distance:
+        if sensor_position_y - distance < line < sensor_position_y + distance:
             result.append(record)
     return result
 
 
-def init_map(report:Report) -> None:
+def init_map(report:Report, target_line:int) -> None:
+    '''
+    attempt to reduce map to one target line
+    '''
+
     global map
     global relative_shift
 
-    min_x, min_y, max_x, max_y = find_edges(report)
-    map_height = max_y - min_y + 1
+    min_x, min_y, max_x, _ = find_edges(report)
     map_width = max_x - min_x + 1
 
-    for _ in range(map_height):
-        map.append([Matter.EMPTY] * map_width)
+    map = [Matter.EMPTY] * map_width
 
-    relative_shift_x, relative_shift_y = min_x, min_y
+    relative_shift_x, relative_shift_y = min_x, target_line - min_y
     relative_shift = relative_shift_x, relative_shift_y
 
     for sensor_position, beacon_position, _ in report:
         sensor_position_x, sensor_position_y, beacon_position_x, beacon_position_y = sensor_position + beacon_position
         sensor_calibrated_x = sensor_position_x - relative_shift_x
-        sensor_calibrated_y = sensor_position_y - relative_shift_y
         beacon_calibrated_x = beacon_position_x - relative_shift_x
-        beacon_calibrated_y = beacon_position_y - relative_shift_y
-        map[sensor_calibrated_y][sensor_calibrated_x] = Matter.SENSOR
-        map[beacon_calibrated_y][beacon_calibrated_x] = Matter.BEACON
+
+        if sensor_position_y == target_line:
+            map[sensor_calibrated_x] = Matter.SENSOR
+        if beacon_position_y == target_line:
+            map[beacon_calibrated_x] = Matter.BEACON
 
 
-def set_coverage(report:Report) -> None:
+def set_coverage(report:Report, target_line:int) -> None:
     global map
     global relative_shift
 
@@ -99,106 +103,65 @@ def set_coverage(report:Report) -> None:
 
         sensor_position_x, sensor_position_y = sensor_position
 
+        actual_target_line = target_line - relative_shift_y
         actual_sensor_position_x = sensor_position_x - relative_shift_x
         actual_sensor_position_y = sensor_position_y - relative_shift_y
 
-        map_height = len(map)
-        map_width = len(map[0])
-
-        if actual_sensor_position_y - distance < 0:
-            'expand map to top if necessary'
-            expand_length = abs(actual_sensor_position_y - distance)
-            for _ in range(expand_length):
-                map.insert(0, [Matter.EMPTY] * map_width)
-
-            relative_shift_y -= expand_length
-            actual_sensor_position_y = sensor_position_y - relative_shift_y
-            relative_shift = relative_shift_x, relative_shift_y
-            map_height = len(map)
+        map_width = len(map)
 
         if actual_sensor_position_x - distance < 0:
             'expand map to left if necessary'
             expand_length = abs(actual_sensor_position_x - distance)
-            for i in range(map_height):
-                map[i] = [Matter.EMPTY] * expand_length + map[i]
-
+            map = [Matter.EMPTY] * expand_length + map
             relative_shift_x -= expand_length
             actual_sensor_position_x = sensor_position_x - relative_shift_x
             relative_shift = relative_shift_x, relative_shift_y
-            map_width = len(map[0])
-
-        if actual_sensor_position_y + distance >= map_height:
-            'expand map to bottom if necessary'
-            expand_length = abs(actual_sensor_position_y + distance - map_height) + 1
-            for _ in range(expand_length):
-                map.append([Matter.EMPTY] * map_width)
-
-            map_height = len(map)
+            map_width = len(map)
 
         if actual_sensor_position_x + distance >= map_width:
             'expand map to right if necessary'
             expand_length = abs(actual_sensor_position_x + distance - map_width) + 1
-            for i in range(map_height):
-                map[i] += [Matter.EMPTY] * expand_length
-
-            map_width = len(map[0])
+            map += [Matter.EMPTY] * expand_length
+            map_width = len(map)
 
         for i in range(distance + 1):
-            for j in range(distance + 1 - i):
-                '↑'
-                covered_y = actual_sensor_position_y - i
-                '←'
-                covered_x = actual_sensor_position_x - j
-                if map[covered_y][covered_x] == Matter.EMPTY:
-                    map[covered_y][covered_x] = Matter.COVERED
+            if actual_sensor_position_y - i == actual_target_line or actual_sensor_position_y + i == actual_target_line:
+                for j in range(distance + 1 - i):
+                    '←'
+                    covered_x = actual_sensor_position_x - j
+                    if map[covered_x] == Matter.EMPTY:
+                        map[covered_x] = Matter.COVERED
 
-                '→'
-                covered_x = actual_sensor_position_x + j
-                if map[covered_y][covered_x] == Matter.EMPTY:
-                    map[covered_y][covered_x] = Matter.COVERED
+                    '→'
+                    covered_x = actual_sensor_position_x + j
+                    if map[covered_x] == Matter.EMPTY:
+                        map[covered_x] = Matter.COVERED
 
-                '↓'
-                covered_y = actual_sensor_position_y + i
-                '←'
-                covered_x = actual_sensor_position_x - j
-                if map[covered_y][covered_x] == Matter.EMPTY:
-                    map[covered_y][covered_x] = Matter.COVERED
 
-                '→'
-                covered_x = actual_sensor_position_x + j
-                if map[covered_y][covered_x] == Matter.EMPTY:
-                    map[covered_y][covered_x] = Matter.COVERED
-
-        # print()
-        # print(sensor_position)
-        # print_map()
-        # print()
+def get_distance_to_possible_next_beacon():
+    global map
+    return len([tile for tile in map if tile == Matter.COVERED])
 
 
 def print_map() -> None:
     global map
-
-    for line in map:
-        print(' '.join([tile.value for tile in line]))
+    print(''.join([tile.value for tile in map]))
 
 
 def main() -> None:
-    input_filename = 'example.txt'
+    input_filename = 'input.txt'
     current_directory = os.path.dirname(__file__)
     report = read_report_from_file(os.path.join(current_directory, input_filename))
 
     # solution for part 1
 
-    target_line = 10
+    target_line = 2000000
     stripped_report = strip_report(report, target_line)
-
-    init_map(stripped_report)
-    set_coverage(stripped_report)
-    print_map()
-
-    print()
-    _, shift_y = relative_shift
-    print(' '.join([tile.value for tile in map[target_line - shift_y]]))
+    init_map(stripped_report, target_line)
+    set_coverage(stripped_report, target_line)
+    print(f"(part 1) {get_distance_to_possible_next_beacon()} positions cannot contain a beacon")
+    
+    # print_map()
 
 
 if __name__ == "__main__":
